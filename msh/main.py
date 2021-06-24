710a2fbc04953d122fda08807b2d5c1c7f007526
a-happy-ma-nong <69619380+a-happy-ma-nong@users.noreply.github.com>
2021-06-24 21:32:03 +0800

    Add files via upload

--- msh/main.py
+++ msh/main.py
@@ -1,5 +1,24 @@
-from msh.cat import *
-from msh.cd import *
-from msh.df import *
-from msh.cpufreq import *
-from msh.ls import *
+try:
+    from msh.cat import *
+    from msh.cd import *
+    from msh.df import *
+    from msh.cpufreq import *
+    from msh.ls import *
+except ImportError:
+    from cat import *
+    from cd import *
+    from df import *
+    from cpufreq import *
+    from ls import *
+import os
+while True:
+    input_print = "micropython %s $" % os.getcwd()
+    command = str(input(input_print))
+    if command == "exit":
+        break
+    elif command == "ls":
+        ls()
+    elif command == "":
+        pass
+    else:
+        print("msh: %s not found" % command)
