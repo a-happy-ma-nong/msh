1570507b3c1f9d30c0759942dbc98c597f21b2f8
a-happy-ma-nong <1fc-hpmo80gadm@dingtalk.com>
2022-01-23 19:24:55 +0800

    Update cd.py

--- msh/cd.py
+++ msh/cd.py
@@ -1,3 +1,6 @@
 def cd(path):
-    import os
-    os.chdir(str(path))
+    try:
+        import os
+        os.chdir(str(path))
+    except FileNotFoundError:
+        print("cd: %s: No such file or directory" % path)
